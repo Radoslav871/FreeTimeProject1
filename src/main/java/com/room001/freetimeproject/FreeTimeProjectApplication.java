d58b7f255dacd28b0a2402362e5e76338eb04ad6
Radoslav Kucerak <Kucerakrgmail.com>
2022-05-18 21:36:05 +0200

    first

---
+++ src/main/java/com/room001/freetimeproject/FreeTimeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.room001.freetimeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeTimeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeTimeProjectApplication.class, args);
+    }
+
+}
