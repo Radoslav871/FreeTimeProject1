2cf02ff080d3297128f965a4fc318f1973f110c2
JohnnyPlv <h.plavka@gmail.com>
2022-06-01 15:56:52 +0200

    basic layout, pop ups implemented

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,35 @@
 import Welcome from "./components/Welcome";
+import LoginPopup from "./components/LoginPopup";
+import SignUpPopup from "./components/SignUpPopup";
+import {useState} from "react";
+
 
 function App() {
-  return <Welcome />;
+  const [ loginIsOpen, setLoginIsOpen] = useState(false);
+  const [ signUpIsOpen, setSignUpIsOpen] = useState(false);
+
+  const openLoginForm = () => {
+    setLoginIsOpen(true);
+  }
+  const closeLoginForm = () => {
+    setLoginIsOpen(false);
+  }
+
+  const openSignUpForm = () => {
+    setSignUpIsOpen(true);
+  }
+  const closeSignUpForm = () => {
+    setSignUpIsOpen(false);
+  }
+
+
+    return (
+        <div>
+          <Welcome passDataLogin={openLoginForm} passDataSignUp={openSignUpForm} />
+          {loginIsOpen ? <LoginPopup data={closeLoginForm} /> : null}
+          {signUpIsOpen ? <SignUpPopup data={closeSignUpForm} /> : null}
+        </div>
+    )
 }
 export default App;
+
